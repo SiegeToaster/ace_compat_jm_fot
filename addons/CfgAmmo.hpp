d333a6e156e66f50eee83caaadaf69bdcb03ae12
Siege <81945479+SiegeToaster@users.noreply.github.com>
2022-04-23 12:11:18 -0700

    All guns and ammo data inputted most of the numbers are completely made up and don't make any sense, but whatever.

--- addons/CfgAmmo.hpp
+++ addons/CfgAmmo.hpp
@@ -9,46 +9,43 @@ class CfgAmmo {
 
 		// idk what these values are but they were in the example
 		ACE_muzzleVelocityVariationSD = .35;
-		ACE_ammoTempMuzzleVelocityShifts[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		ACE_ammoTempMuzzleVelocityShifts[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // ToDo: find values for these
 
-		ACE_muzzleVelocities[] = {795}; // ToDo: match with each gun's barrel length
-		ACE_barrelLengths[] = {280}; // ToDo: add each gun's barrel length to this list
+		ACE_muzzleVelocities[] = {795, 795, 795, 795}; // ToDo: match with each gun's barrel length
+		ACE_barrelLengths[] = {280, 290, 475, 508}; // ToDo: add each gun's barrel length to this list
 	};
 	class JMSFALL_762x51_Ball: BulletBase {
 		ACE_caliber = 7.62;
 		ACE_bulletLength = 51;
 		ACE_bulletMass = 10;
 
-		// idk what these values are but they were in the example
 		ACE_muzzleVelocityVariationSD = .35;
 		ACE_ammoTempMuzzleVelocityShifts[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-		ACE_muzzleVelocities[] = {}; // ToDo: match with each gun's barrel length
-		ACE_barrelLengths[] = {}; // ToDo: add each gun's barrel length to this list
+		ACE_muzzleVelocities[] = {795, 795};
+		ACE_barrelLengths[] = {300, 485};
 	};
 	class JMSFALL_45ACP_Ball: BulletBase {
 		ACE_caliber = 11.5;
 		ACE_bulletLength = 22.8;
 		ACE_bulletMass = 12; // huh?
 
-		// idk what these values are but they were in the example
 		ACE_muzzleVelocityVariationSD = .35;
 		ACE_ammoTempMuzzleVelocityShifts[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-		ACE_muzzleVelocities[] = {}; // ToDo: match with each gun's barrel length
-		ACE_barrelLengths[] = {}; // ToDo: add each gun's barrel length to this list
+		ACE_muzzleVelocities[] = {370, 370};
+		ACE_barrelLengths[] = {127, 150};
 	};
 	class JMSFALL_145x114_Ball: BulletBase {
 		ACE_caliber = 14.88;
 		ACE_bulletLength = 114;
 		ACE_bulletMass = 60;
 
-		// idk what these values are but they were in the example
 		ACE_muzzleVelocityVariationSD = .35;
 		ACE_ammoTempMuzzleVelocityShifts[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-		ACE_muzzleVelocities[] = {}; // ToDo: match with each gun's barrel length
-		ACE_barrelLengths[] = {}; // ToDo: add each gun's barrel length to this list
+		ACE_muzzleVelocities[] = {800};
+		ACE_barrelLengths[] = {1100};
 	};
 
 	class RocketBase;
@@ -61,8 +58,8 @@ class CfgAmmo {
 			maxDeflection = 0.001;
 			incDeflection = 0.0005;
 			canVanillaLock = 1;
-			defaultSeekerType = "SACLOS";
-			seekerTypes[] = {"SALH", "SACLOS"}; // ?
+			defaultSeekerType = "SACLOS"; // ToDo: determine 
+			seekerTypes[] = {"SALH", "SACLOS"}; // semi-active laser homing; semi-automatic command to line of sight
 			defaultSeekerLockMode = "LOAL";
 			seekerLockModes[] = {"LOAL", "LOBL"}; // lock on after launch; lock on before launch
 			seekerAngle = 90;
@@ -83,7 +80,7 @@ class CfgAmmo {
 			defaultSeekerType = "SACLOS";
 			seekerTypes[] = {"SALH", "SACLOS"}; // ?
 			defaultSeekerLockMode = "LOAL";
-			seekerLockModes[] = {"LOAL", "LOBL"}; // lock on after launch; lock on before launch
+			seekerLockModes[] = {"LOAL", "LOBL"};
 			seekerAngle = 90;
 			seekerAccuracy = 1;
 			seekerMinRange = 1;
--- addons/CfgWeapons.hpp
+++ addons/CfgWeapons.hpp
@@ -221,6 +221,31 @@ class CfgWeapons {
 
 
 	// ========== Weapons ==========
+	class Pistol;
+	class Pistol_Base_F: Pistol {};
+	class JMSFALL_P2015_Pistol: Pistol_Base_F {
+		ACE_barrelTwist = 406;
+		ACE_barrelLength = 127;
+		ACE_overheating_mrbs = 1000;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 1;
+	};
+	class JMSFALL_RE45_Pistol: Pistol_Base_F {
+		ACE_barrelTwist = 406;
+		ACE_barrelLength = 150;
+		ACE_overheating_mrbs = 1000;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 1;
+	};
+
 	class Rifle_Base_F;
 	class JMSFALL_Rifle_Base: Rifle_Base_F {};
 	class JMSFALL_car101Rifle: JMSFALL_Rifle_Base {
@@ -232,5 +257,117 @@ class CfgWeapons {
 		ACE_overheating_dispersion = 0.75;
 		ACE_arsenal_hide = 0;
 		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 0;
+	}; // Make sure that the grey rifle also take these settings
+	class JMSFALL_m1a3Rifle: JMSFALL_Rifle_Base {
+		ACE_barrelTwist = 201.3;
+		ACE_barrelLength = 300;
+		ACE_overheating_mrbs = 2500;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 0;
+	}; // make sure that the gray/black rifles also take these settings
+	class JMSFALL_r97Rifle: JMSFALL_Rifle_Base {
+		ACE_barrelTwist = 201.3;
+		ACE_barrelLength = 290;
+		ACE_overheating_mrbs = 3000;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 0;
+	}; // make sure that the black/blue rifles also take these settings
+	class JMSFALL_r101Rifle: JMSFALL_Rifle_Base {
+		ACE_barrelTwist = 215.6;
+		ACE_barrelLength = 475;
+		ACE_overheating_mrbs = 2000;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 0;
+	}; // make sure that the black/blue/brown/white rifles also take these settings
+	class JMSFALL_V47flatlineRifle: JMSFALL_Rifle_Base {
+		ACE_barrelTwist = 215.6;
+		ACE_barrelLength = 485;
+		ACE_overheating_mrbs = 2000;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 0;
+	}; // make sure that the black rifle also takes these settings
+
+	class Rifle_Long_Base_F;
+	class JMSFALL_SniperRifle_Base: Rifle_Long_Base_F {};
+	class JMSFALL_d101Rifle: JMSFALL_SniperRifle_Base {
+		ACE_barrelTwist = 230;
+		ACE_barrelLength = 508;
+		ACE_overheating_mrbs = 1750;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 1;
+	}; // make sure that the blue rifle also takes these settings
+	class JMSFALL_KraberAPRifle: JMSFALL_SniperRifle_Base {
+		ACE_barrelTwist = 482.7;
+		ACE_barrelLength = 1100;
+		ACE_overheating_mrbs = 5000;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 1;
+	};
+	class JMSFALL_spitfireRifle: Rifle_Long_Base_F {
+		ACE_barrelTwist = 230;
+		ACE_barrelLength = 485;
+		ACE_overheating_mrbs = 1750;
+		ACE_overheating_slowdownFactor = 1;
+		ACE_overheating_allowSwapBarrel = 0;
+		ACE_overheating_dispersion = 0.75;
+		ACE_arsenal_hide = 0;
+		ACE_twistDirection = 1;
+		ace_overheating_closedBolt = 0;
+	}; // make sure that the hex rifle also takes these settings
+
+	class Launcher;
+	class Launcher_Base_F: Launcher {};
+	class JMSFALL_Archer_launcher: Launcher_Base_F {
+		ace_overpressure_angle = 60;
+		ace_overpressure_range = 10;
+		ace_overpressure_damage = 0.7;
+		ace_reloadlaunchers_enabled = 1;
+	};
+
+	class autocannon_35mm;
+	class JMSFALL_va_goblin_35mm: autocannon_35mm {
+		ACE_overpressure_angle = 0;
+		ACE_overpressure_damage = 0;
+		ACE_overpressure_priority = 0;
+		ACE_overpressure_range = 0;
+	};
+
+	class Gatling_30mm_Plane_CAS_01_F;
+	class JMSFALL_va_goblin_gatling_30mm: Gatling_30mm_Plane_CAS_01_F {
+		ACE_overpressure_angle = 0;
+		ACE_overpressure_damage = 0;
+		ACE_overpressure_priority = 0;
+		ACE_overpressure_range = 0;
+	};
+	class JMSFALL_va_crow_gatling_35mm: Gatling_30mm_Plane_CAS_01_F {
+		ACE_overpressure_angle = 0;
+		ACE_overpressure_damage = 0;
+		ACE_overpressure_priority = 0;
+		ACE_overpressure_range = 0;
 	};
 };
