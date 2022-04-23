8b7b12850c1ff8658d45e9d49018622e131e83e1
Siege <81945479+SiegeToaster@users.noreply.github.com>
2022-04-22 19:05:34 -0700

    Initial file structure setup

---
+++ addons/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches
+{
+	class ace_compat_jm_fot
+	{
+		name = "ACE3 JM's Fall of the Titans Compatability";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {}; // Find out how to get FOT in here
+		authors[] = {"Siege"};
+	};
+};
+
+#include "CfgAmmo.hpp"
+#include "CfgGlasses.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
---
+++ mod.cpp
@@ -0,0 +1,5 @@
+name = "ACE3 - JM's Fall of the Titans Compatability";
+author = "Siege";
+actionName = "GitHub";
+action = "https://github.com/SiegeToaster/FoT_ACE_Compat";
+overview = "ACE Compatability patch for JM's Fall of the Titans.";
